eac725e60eb9834f25da94d7bf0781a0795388a8
Eya Gargah <eyagargah@hotmail.com>
2023-05-15 21:22:01 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { BoardComponent } from './pages/board/board.component';
+import { TaskComponent } from './pages/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BoardComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
