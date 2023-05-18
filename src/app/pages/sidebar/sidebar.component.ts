69df2a6659fc60d33204eca691bbb8a451321403
Eya Gargah <eyagargah@hotmail.com>
2023-05-18 22:14:52 +0100

    Update sidebar.component.ts

--- src/app/pages/sidebar/sidebar.component.ts
+++ src/app/pages/sidebar/sidebar.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
-
-  itoogle : boolean = false
-  toggleMenu(){
-    console.log('clicked')
-    this.itoogle = !this.itoogle
+ 
+  itoogle: boolean = false;
+  toggleMenu() {
+    alert('clicked')
+    this.itoogle = !this.itoogle;
+    
   }
+
+
+  dropDownMenu(){
+    const dropdownMenu = document.querySelector('.dropdownContent')
+
+    if(dropdownMenu?.classList.contains('hide')){
+      dropdownMenu.classList.remove('hide')
+    }else {
+      dropdownMenu?.classList.add('hide')
+    }
+  
+  }
+ 
 }
