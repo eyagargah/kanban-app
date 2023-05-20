fe8f9e22c7b5ea4ec377bc27477c9cf75260c842
Eya Gargah <eyagargah@hotmail.com>
2023-05-20 22:33:32 +0100

    Create board-modal.component.ts

---
+++ src/app/pages/board-modal/board-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-modal',
+  templateUrl: './board-modal.component.html',
+  styleUrls: ['./board-modal.component.scss']
+})
+export class BoardModalComponent {
+
+}
