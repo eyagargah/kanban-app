4f9bbe4d91c474e9986dba9bd52a547284b64b73
Eya Gargah <eyagargah@hotmail.com>
2023-05-20 22:32:00 +0100

    Update sidebar.component.ts

--- src/app/pages/sidebar/sidebar.component.ts
+++ src/app/pages/sidebar/sidebar.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { TaskComponent } from '../task/task.component';
 import { TaskModalComponent } from '../task-modal/task-modal.component';
+import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
+  sideNavOpened : boolean | undefined 
   fileNameDialogRef: MatDialogRef<TaskComponent> | undefined;
 
   constructor(private dialog: MatDialog) {}
@@ -24,17 +26,20 @@ export class SidebarComponent {
 
   showSideBar() {
     const showSidebar = document.querySelector('.showSidebar');
+ 
     if (showSidebar?.classList.contains('show')) {
       showSidebar.classList.remove('show');
       showSidebar.classList.add('hide');
+      this.sideNavOpened = true
     }
   }
   hideSideNav() {
     const showSidebar = document.querySelector('.showSidebar');
-
+    
     if (showSidebar?.classList.contains('hide')) {
       showSidebar.classList.remove('hide');
       showSidebar.classList.add('show');
+      this.sideNavOpened = false
     }
   }
 
