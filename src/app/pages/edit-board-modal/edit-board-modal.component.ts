c35eb65ff7808984ec4b8f8e4d6a1598cfc5c271
Eya Gargah <eyagargah@hotmail.com>
2023-05-21 18:21:55 +0100

    create edit board modal

---
+++ src/app/pages/edit-board-modal/edit-board-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-board-modal',
+  templateUrl: './edit-board-modal.component.html',
+  styleUrls: ['./edit-board-modal.component.scss']
+})
+export class EditBoardModalComponent {
+
+}
