b81d5f695458d0b387c228a360c26c9005847837
Eya Gargah <eyagargah@hotmail.com>
2023-05-24 21:02:50 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/header/header.component';
@@ -9,17 +10,19 @@ import { BoardComponent } from './pages/board/board.component';
 import { TaskComponent } from './pages/task/task.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule } from '@angular/material/sidenav';
-import {MatIconModule } from '@angular/material/icon';
-import {MatListModule } from '@angular/material/list';
-import {MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 import { addTaskModalComponent } from './pages/add-task-modal/add-task-modal.component';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { BoardModalComponent } from './pages/board-modal/board-modal.component';
 import { EditBoardModalComponent } from './pages/edit-board-modal/edit-board-modal.component';
 import { DeleteBoardModalComponent } from './pages/delete-board-modal/delete-board-modal.component';
 import { ViewtaskModalComponent } from './pages/viewtask-modal/viewtask-modal.component';
+import { ColumnComponent } from './pages/column/column.component';
+import { SubtaskComponent } from './pages/subtask/subtask.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +34,9 @@ import { ViewtaskModalComponent } from './pages/viewtask-modal/viewtask-modal.co
     BoardModalComponent,
     EditBoardModalComponent,
     DeleteBoardModalComponent,
-    ViewtaskModalComponent
+    ViewtaskModalComponent,
+    ColumnComponent,
+    SubtaskComponent,
   ],
   imports: [
     BrowserModule,
@@ -44,9 +49,10 @@ import { ViewtaskModalComponent } from './pages/viewtask-modal/viewtask-modal.co
     MatButtonModule,
     MatListModule,
     MatDialogModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
