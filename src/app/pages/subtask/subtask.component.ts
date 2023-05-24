874f503615f8ff91b721b3ef23e2ac70e4f43bb9
Eya Gargah <eyagargah@hotmail.com>
2023-05-24 21:03:28 +0100

    Create subtask.component.ts

---
+++ src/app/pages/subtask/subtask.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subtask',
+  templateUrl: './subtask.component.html',
+  styleUrls: ['./subtask.component.scss']
+})
+export class SubtaskComponent {
+
+}
