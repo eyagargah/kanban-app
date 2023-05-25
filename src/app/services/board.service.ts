738c16abc705ec8ca23ca7ef9f9495e1643f831a
Eya Gargah <eyagargah@hotmail.com>
2023-05-25 21:23:05 +0100

    Update board.service.ts

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@angular/core';
-import data from 'src/assets/data.json'
 @Injectable({
   providedIn: 'root'
 })
 export class BoardService {
 
-  boardsData = data
-  boards = this.boardsData[0].boards
   constructor() { }
 
   
