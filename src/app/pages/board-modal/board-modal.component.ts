85bfe2eae8bf936811ea00cb8bec837d6102a034
Eya Gargah <eyagargah@hotmail.com>
2023-05-25 19:58:19 +0100

    Update board-modal.component.ts

--- src/app/pages/board-modal/board-modal.component.ts
+++ src/app/pages/board-modal/board-modal.component.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-board-modal',
   templateUrl: './board-modal.component.html',
-  styleUrls: ['./board-modal.component.scss']
+  styleUrls: ['./board-modal.component.scss'],
 })
 export class BoardModalComponent {
-  boardForm= this.fb.group({
-    name:'',
-    columns:[
-      
-    ]
-
-  })
-  constructor(private fb: FormBuilder){}
+  public boardForm: FormGroup;
+  constructor(private _fb: FormBuilder) {
+    this.boardForm = this._fb.group({
+      columns: this._fb.array([this.addColumnGroup()])
+    });
+  }
+  //Append Fields Set
+  private addColumnGroup(): FormGroup {
+    return this._fb.group({
+      column:''
+    });
+  }
+  //Add Fields
+  addColumn(): void {
+    this.columnArray.push(this.addColumnGroup());
+  }
+ 
+  //Remove Fields
+  removeColumn(index: number): void {
+    this.columnArray.removeAt(index);
+  }
+  //Fields Array
+  get columnArray(): FormArray {
+    return <FormArray>this.boardForm.get('columns');
+  }
+  onSubmit(): void {
+    console.log('submitted form' + JSON.stringify(this.boardForm?.value));
+  }
 }
