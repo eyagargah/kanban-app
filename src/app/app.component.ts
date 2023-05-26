d79911e1bcdd33c9e5c81408033f414c578c1323
Eya Gargah <eyagargah@hotmail.com>
2023-05-26 16:48:37 +0100

    Update app.component.ts
    fetch data from json file and set them in localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { BoardService } from './services/board.service';
+import data from 'src/assets/data.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kanban-app';
+  constructor( private boardService: BoardService){
+    
+  }
+  ngOnInit(){
+    if(localStorage.getItem('boards') === null)
+    {
+      this.boardService.setBoards(data)
+    }
+    this.boardService.getBoards()
+    this.boardService.setCurrentBoard(this.boardService.boards.boards[0])
+  }
 }
