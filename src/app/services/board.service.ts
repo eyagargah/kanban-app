1fc24eb6fe3bab5c6f74e5015aa6cc6f87da05c2
Eya Gargah <eyagargah@hotmail.com>
2023-05-26 16:48:55 +0100

    Update board.service.ts

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -1,10 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Board, Boards, Task } from 'src/app/boards.interface';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BoardService {
+  constructor() {}
+  boards!: Boards;
+  currentBoard!: Board;
+  currentTask: Task = {
+    title: 'string',
+    description: 'string',
+    status: 'string',
+    subtasks: [],
+  };
 
-  constructor() { }
+  indexes = {
+    boardIndex: 0,
+    columnIndex: 0,
+    taskIndex: 0,
+    subtaskIndex: 0,
+    dropColumnIndex: 0,
+    dropTaskIndex: 0,
+  };
 
-  
+  getBoards() {
+    this.boards = JSON.parse(localStorage['boards']);
+  }
+  setBoards(boards: Boards) {
+    localStorage.setItem('boards', JSON.stringify(boards));
+  }
+
+  setCurrentBoard(board: Board) {
+    this.currentBoard = board;
+  }
+  setCurrentTask(task: Task) {
+    this.currentTask = task;
+  }
 }
